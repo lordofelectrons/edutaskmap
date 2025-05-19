046237218cf76a8ad8ae9541ec735540cf59c640
Artem Filipenko <artemnayamateria@gmail.com>
2025-05-19 20:31:51 +0300

    Start using eslint for backend and fix startup script

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import { Pool } from 'pg';
 import cors from 'cors';
-import { json } from 'body-parser';
+import bodyParser from 'body-parser';
 
 const app = express();
 const port = 3001;
 
 app.use(cors());
-app.use(json());
+app.use(bodyParser.json());
 
 const pool = new Pool({
   user: 'admin',
