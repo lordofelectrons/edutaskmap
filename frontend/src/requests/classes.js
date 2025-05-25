7a9f75fbe228147132627c35b67367528607d2f3
Artem Filipenko <artemnayamateria@gmail.com>
2025-05-25 18:47:21 +0300

    Fix the proxy init

--- frontend/src/requests/classes.js
+++ frontend/src/requests/classes.js
@@ -1,11 +1,11 @@
 export async function fetchClassesBySchoolAndGrade(school_name, grade, cb) {
-  const res = await fetch(`/classes/by-school-and-grade?school_name=${encodeURIComponent(school_name)}&grade=${grade}`);
+  const res = await fetch(`/api/classes/by-school-and-grade?school_name=${encodeURIComponent(school_name)}&grade=${grade}`);
   const data = await res.json();
   cb(data);
 }
 
 export async function addClass({ grade, name, school_id }, cb) {
-  const res = await fetch('classes', {
+  const res = await fetch('/api/classes', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ grade, name, school_id }),
